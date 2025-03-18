eaeadcb93c0d7d010f300d9448fdc760972466d4
Revoj-a <revojjoever@gmail.com>
2025-03-18 21:57:55 +0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 12;
+export const KEY = 'e3a66c93-f533-4f95-aab3-3d771dd34574';
+export const MODAL_CLOSE_SEC = 2.5;
